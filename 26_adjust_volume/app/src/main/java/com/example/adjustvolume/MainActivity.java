5cbede53b6fe8b37d8ae5a415eae3b2ed7c68896
Gabriela <gabriela.cleims@gmail.com>
2024-07-15 23:02:27 -0300

    new project

---
+++ 26_adjust_volume/app/src/main/java/com/example/adjustvolume/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.adjustvolume;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+/*Crie um app com um slider (SeekBar) que simula o ajuste de volume e exibe o valor.*/
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        seekBar = findViewById(R.id.seek);
+        textView = findViewById(R.id.txt);
+
+        // Configurar um listener para quando o valor da SeekBar é alterado
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // Atualizar o TextView com o valor atual do volume
+                textView.setText("Volume: " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+}
